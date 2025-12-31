5698c19de8da4e2a0e8a87c2cd7d56e060c27c1b
JunTae Son <sonjt0705@hanmail.net>
2025-12-31 21:41:20 +0900

    Removed unnecessary code in kernel start function for testing from UEFI boot loader. Removed header in kernel binary format for simple testing.

--- src/knentry.c
+++ src/knentry.c
@@ -1,17 +1,4 @@
-#define TEXT_OUTPUT_ADDRESS 0x0000000004000000
-
-typedef struct _TEXT_OUTPUT TEXT_OUTPUT;
-
-struct _TEXT_OUTPUT {
-	unsigned long long unused1;
-	unsigned long long (*output_string)(TEXT_OUTPUT*, unsigned short*);
-};
-
-void kputs(unsigned short* buffer) {
-	TEXT_OUTPUT* text_output = (TEXT_OUTPUT*)TEXT_OUTPUT_ADDRESS;
-	text_output->output_string(text_output, buffer);
-}
-
 void _start(void) {
-	kputs(L"Hello, kernel!\r\n");
+	int a = 0;
+	while (a < 1024) a++;
 }
